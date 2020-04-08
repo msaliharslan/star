043861fbdfd844904c929e496e773ee5bf407cee
salih <m_salih_arslan@hotmail.com>
2020-04-08 14:39:32 +0300

    fisheye undistortion done, quality check required

--- Processor/FisheyePlayground/main.py
+++ Processor/FisheyePlayground/main.py
@@ -7,9 +7,7 @@ import cv2
 os.chdir("..")
 sys.path.append(os.getcwd())
 
-import matplotlib.pyplot as plot
-
-
+import matplotlib.pyplot as plt
 import fetcher
 
 fetcher.insertFocusDataPath("fisheyeAtakan")
@@ -17,15 +15,12 @@ fetcher.fetchFisheyeDataT265()
 
 targetFrameIndex = 200
 
-fisheyeWidth_a = 800
-fisheyeHeight_a = 848
+fisheyeWidth = 800
+fisheyeHeight = 848
 
-fisheyeWidth_b = 848
-fisheyeHeight_b = 800
 
 data = np.array((fetcher.fisheye1_t265[targetFrameIndex][1:-1]).split(", "),dtype=np.uint8)
-data = data.reshape((fisheyeWidth_b,fisheyeHeight_b))
-data = data.reshape((fisheyeWidth_a,fisheyeHeight_a))
+data = data.reshape((fisheyeWidth,fisheyeHeight))
 
 
 K1 = np.array([[284.501708984375, 0.0, 430.9294128417969], [0.0, 285.4164123535156, 394.66510009765625], [0.0, 0.0, 1.0]])
@@ -35,19 +30,19 @@ D1 = np.array([-0.00012164260260760784, 0.03437558934092522, -0.0325258299708366
 D2 = np.array([0.0009760634857229888, 0.030147459357976913, -0.02769969031214714, 0.0031066760420799255])
 
 
-undistorted = cv2.fisheye.undistortImage(data, K=K1, D=D1)
+undistorted = cv2.fisheye.undistortImage(data, K=K1, D=D1, Knew=K1)
 
-cv2.imshow("title", undistorted.reshape((fisheyeWidth_a, fisheyeHeight_a)))
-cv2.imshow("title2", data.reshape((fisheyeWidth_a, fisheyeHeight_a)))
+plt.imshow( undistorted, cmap='gray')
+# plt.imshow( data.reshape((fisheyeWidth_a, fisheyeHeight_a)))
 
 
 
-m1type = cv2.CV_32FC1
-(lm1, lm2) = cv2.fisheye.initUndistortRectifyMap(K1, D1, np.eye(3), np.eye(3), (300,300), m1type)
+# m1type = cv2.CV_32FC1
+# (lm1, lm2) = cv2.fisheye.initUndistortRectifyMap(K1, D1, np.eye(3), np.eye(3), (300,300), m1type)
 
-godPls =cv2.remap(src = data ,map1 = lm1,map2 = lm2,interpolation = cv2.INTER_LINEAR)
+# godPls =cv2.remap(src = data ,map1 = lm1,map2 = lm2,interpolation = cv2.INTER_LINEAR)
 
-cv2.imshow("title", godPls)
+# plt.imshow(godPls)
 
 
 print(fetcher.fisheye1_t265.shape)
